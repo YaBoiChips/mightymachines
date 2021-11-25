e75d11c2a9a40fc91a16cfd13c4e9e44f7c2b131
vexxj <craftwizardj@gmail.com>
2021-11-24 23:28:07 -0500

    added ores

--- src/main/java/yaboichips/mightymachines/core/MMBlocks.java
+++ src/main/java/yaboichips/mightymachines/core/MMBlocks.java
@@ -13,6 +13,18 @@ import java.util.List;
 public class MMBlocks {
     public static List<Block> blocks = new ArrayList<>();
 
+    //ores
+    public static final Block RUBY_ORE = createOreBlock("ruby_ore");
+    public static final Block AMETHYST_ORE = createOreBlock("amethyst_ore");
+    public static final Block SAPPHIRE_ORE = createOreBlock("sapphire_ore");
+    public static final Block TOPAZ_ORE = createOreBlock("topaz_ore");
+    public static final Block OPAL_ORE = createOreBlock("opal_ore");
+    public static final Block AMBER_ORE = createOreBlock("amber_ore");
+    public static final Block ONYX_ORE = createOreBlock("onyx_ore");
+    public static final Block PYRITE_ORE = createOreBlock("pyrite_ore");
+    public static final Block THALLIUM_ORE = createOreBlock("thallium_ore");
+    public static final Block BAUXITE_ORE = createOreBlock("bauxite_ore");
+    public static final Block TIN_ORE = createOreBlock("tin_ore");
 
 
     static @Nonnull Block createOreBlock(String id) {
