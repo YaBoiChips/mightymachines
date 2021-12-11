3c7ca66ad659f84e303e5eb6589793964c845145
YaBoiChips <63213388+YaBoiChips@users.noreply.github.com>
2021-12-10 18:21:20 -0700

    ported to 1.18.1

--- src/main/java/yaboichips/mightymachines/common/items/guns/PyriteGun.java
+++ src/main/java/yaboichips/mightymachines/common/items/guns/PyriteGun.java
@@ -1,6 +1,7 @@
 package yaboichips.mightymachines.common.items.guns;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -16,13 +17,12 @@ public class PyriteGun extends MiningGun {
     public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
         ItemStack stack = player.getItemInHand(hand);
         if (world.isClientSide) return InteractionResultHolder.fail(stack);
-        if (this.getBlockLookingAt(player) != null) {
-            for (int x1 = this.getBlockLookingAt(player).getX() - 1; x1 <= this.getBlockLookingAt(player).getX() + 1; ++x1) {
-                for (int y1 = this.getBlockLookingAt(player).getY() - 1; y1 <= this.getBlockLookingAt(player).getY() + 1; ++y1) {
-                    for (int z1 = this.getBlockLookingAt(player).getZ() - 1; z1 <= this.getBlockLookingAt(player).getZ() + 1; ++z1) {
-                        world.destroyBlock(new BlockPos(x1, y1, z1), true);
-                    }
-                }
+        BlockPos pos = this.getBlockLookingAt(player);
+        if (pos != null) {
+            for (Direction facing : Direction.values()) {
+                BlockPos relative = pos.relative(facing);
+                world.destroyBlock(relative, true);
+                world.destroyBlock(pos, true);
             }
         }
         return InteractionResultHolder.success(stack);
